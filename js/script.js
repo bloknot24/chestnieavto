50fa91c97be698f4312a915d3618b48ba35b83ad
Alex Ignatovich <sasha_2804@mail.ru>
2019-08-14 17:41:38 +0300

    Работа над главной страницей

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 $( function() {
-    $( "#accordion" ).accordion();
+	$( "#accordion" ).accordion();
 });
+
 (function($){
-  $(function() {
-    $('.menu__icon').on('click', function() {
-      $(this).closest('.hamburg__menu').toggleClass('menu_state_open');
-    });
-  });
+	$(function() {
+		$('.menu__icon').on('click', function() {
+		$(this).closest('.hamburg__menu').toggleClass('menu_state_open');
+		});
+	});
 })(jQuery);
+
+$(document).ready(function(){
+	$('.slider').bxSlider({
+		responsive: true,
+    	touchEnabled: true,
+    	controls: true,
+    	auto: true,
+    	pause: 6000
+	});
+});
